01d65220c3b530e60867b7a98913faee74021a2b
Ивенин Дмитрий <ivenindp@lad24.ru>
2022-03-12 00:22:34 +0300

    fixc

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ type AppConfig struct {
 	ServerAddress   string `env:"SERVER_ADDRESS"  envDefault:"localhost:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
-	Db              string `env:"DATABASE_CONNECTION_STRING"`
+	DB              string `env:"DATABASE_CONNECTION_STRING"`
 }
 
 type Service struct {
