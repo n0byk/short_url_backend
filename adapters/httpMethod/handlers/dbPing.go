6bc47869b7f41c27dd12635da364a21011662ca0
Ивенин Дмитрий <ivenindp@lad24.ru>
2022-03-18 23:33:38 +0300

    1

--- adapters/httpMethod/handlers/dbPing.go
+++ adapters/httpMethod/handlers/dbPing.go
@@ -1,19 +1,11 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
-
-	config "github.com/n0byk/short_url_backend/config"
 )
 
 func DBPing(w http.ResponseWriter, r *http.Request) {
-	err := config.AppService.Storage.DBPing()
-	if err != nil {
-		log.Println("Can't DBPing")
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("200"))
 }
