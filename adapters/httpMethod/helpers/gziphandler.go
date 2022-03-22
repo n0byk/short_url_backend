00c620cbc8014c9b1b5f479a5753affabdd56611
Ивенин Дмитрий <ivenindp@lad24.ru>
2022-03-22 22:48:55 +0300

    9

--- adapters/httpMethod/helpers/gziphandler.go
+++ adapters/httpMethod/helpers/gziphandler.go
@@ -26,7 +26,6 @@ type gzipResponseWriter struct {
 
 func (w *gzipResponseWriter) WriteHeader(status int) {
 	w.Header().Del("Content-Length")
-	w.ResponseWriter.WriteHeader(status)
 }
 
 func (w *gzipResponseWriter) Write(b []byte) (int, error) {
