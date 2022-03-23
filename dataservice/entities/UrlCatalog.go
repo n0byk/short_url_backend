f7af241701928e509719dca9cf48fbc2d91b039e
Ивенин Дмитрий <ivenindp@lad24.ru>
2022-03-23 11:22:05 +0300

    1

--- dataservice/entities/UrlCatalog.go
+++ dataservice/entities/UrlCatalog.go
@@ -3,5 +3,5 @@ package entities
 //Структура будущей таблицы
 type URLCatalog struct {
 	ShortURL string `json:"short_url"`
-	FullURL  string `json:"full_url"`
+	FullURL  string `json:"original_url"`
 }
