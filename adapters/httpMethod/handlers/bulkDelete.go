6f81495ea30f760094d7275aab8cc024440af854
Ивенин Дмитрий <ivenindp@lad24.ru>
2022-04-05 21:24:11 +0300

    14

--- adapters/httpMethod/handlers/bulkDelete.go
+++ adapters/httpMethod/handlers/bulkDelete.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 
 	config "github.com/n0byk/short_url_backend/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func BulkDelete(w http.ResponseWriter, r *http.Request) {
@@ -34,17 +33,11 @@ func BulkDelete(w http.ResponseWriter, r *http.Request) {
 		w.Write(nil)
 		return
 	}
-	g, _ := errgroup.WithContext(r.Context())
 
-	g.Go(func() error {
-		return config.AppService.Storage.BulkDelete(ids, userID.Value)
-	})
-
-	if err = g.Wait(); err != nil {
-		log.Println("Can't delete items ", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
+	go func() {
+		log.Println("done")
+		config.AppService.Storage.BulkDelete(ids, userID.Value)
+	}()
 
 	log.Println("responce")
 	w.WriteHeader(http.StatusAccepted)
